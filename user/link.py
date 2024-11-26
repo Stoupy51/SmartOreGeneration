21a541a579fcf13b44e3be33d3fa0d0db7394fd9
Stoupy51 <stoupy51@gmail.com>
2024-11-26 17:56:48 +0100

    refactor: ♻️ Moved a slot file from merge folder to link.py for dynamic version check

--- user/link.py
+++ user/link.py
@@ -9,10 +9,10 @@ from config import *
 def main(config: dict) -> None:
 	namespace: str = config["namespace"]
 	version: str = config["version"]
-	functions: str = f"{config['datapack_functions']}/v{version}"
+	major, minor, patch = version.split(".")
 
 	# Write additional confirm load things
-	write_to_file(f"{functions}/load/confirm_load.mcfunction", f"""
+	write_to_load_file(config, f"""
 # Objectives initialization
 scoreboard objectives add {namespace}.data dummy
 scoreboard players set #10 {namespace}.data 10
@@ -27,7 +27,7 @@ schedule function {namespace}:v{version}/configuration/world_bottom_start 5s
 """)
 	
 	# Write second_5 function
-	write_to_file(f"{functions}/second_5.mcfunction", f"""
+	write_to_versioned_file(config, "second_5", f"""
 ## Execute on players
 execute at @a run function {namespace}:v{version}/technical/player
 execute if score #generated {namespace}.data matches 1 run function {namespace}:v{version}/technical/post_generation
@@ -38,14 +38,20 @@ scoreboard players set #generated {namespace}.data 0
 # execute as @e[limit=64] at @s run function {namespace}:v{version}/technical/generate/start
 """)
 	
+	# Random position slot
+	write_to_versioned_file(config, "slots/random_position", f"""
+# Launch the function if is the right version
+execute if score #{namespace}.major load.status matches {major} if score #{namespace}.minor load.status matches {minor} if score #{namespace}.patch load.status matches {patch} run function {namespace}:v{version}/slots/random_position/launch
+""")
+	
 	# Copy every file in the manual_merge folder
 	MANUAL_MERGE_FOLDER: str = f"{ROOT}/manual_merge"
 	for root, _, files in os.walk(MANUAL_MERGE_FOLDER):
 		for file in files:
-			src = f"{root}/{file}"
-			dst = src.replace(MANUAL_MERGE_FOLDER, f"{config['build_datapack']}").replace("VERSION", f"v{version}")
+			src: str = f"{root}/{file}"
+			dst: str = src.replace(MANUAL_MERGE_FOLDER, f"{config['build_datapack']}").replace("VERSION", f"v{version}")
 			with super_open(src, "r") as f:
-				content = f.read()
+				content: str = f.read()
 				content = content.replace("NAMESPACE", namespace)
 				content = content.replace("VERSION", f"v{version}")
 				write_to_file(dst, content)
