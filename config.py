47ac51d068edc14d02719562cc8ebfb0e2a9159e
Stoupy51 <stoupy51@gmail.com>
2024-11-26 17:33:40 +0100

    style: 🎨 Added datapack icon

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ IGNORE_UNSET: bool = True							# If True, the program will ignore unset optionn
 
 
 # Folders
+ASSETS_FOLDER: str = f"{ROOT}/assets"				# Folder where the assets are stored
 BUILD_FOLDER: str = f"{ROOT}/build"					# Folder where the final datapack and resource pack are built
-BUILD_COPY_DESTINATIONS: tuple[list, list] = (["E:/my_folders/advanced_desktop/Python Datapack/src/python_datapack/dependencies/datapack/Smart Ore Generation.zip"], [])	# Can be empty lists if you don't want to copy the generated files
+BUILD_COPY_DESTINATIONS: tuple[list, list] = (["E:/my_folders/advanced_desktop/python_datapack/src/python_datapack/dependencies/datapack/Smart Ore Generation.zip"], [])	# Can be empty lists if you don't want to copy the generated files
 
 # Datapack related constants
 AUTHOR: str = "Stoupy51"					# Author(s) name(s) displayed in pack.mcmeta, also used to add convention.debug tag to the players of the same name(s) <-- showing additionnal displays like datapack loading
@@ -21,6 +22,7 @@ DESCRIPTION = f"{PROJECT_NAME} [{VERSION}] by {AUTHOR}"	# Pack description displ
 configuration = {
 	"ignore_unset": IGNORE_UNSET,
 
+	"assets_folder": ASSETS_FOLDER,
 	"build_folder": BUILD_FOLDER,
 	"build_copy_destinations": BUILD_COPY_DESTINATIONS,
 	"author": AUTHOR,
