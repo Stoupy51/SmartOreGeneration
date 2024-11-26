9b3d0009ace5ddb3f785ec8a56f8f16d4723ad28
Stoupy51 <stoupy51@gmail.com>
2024-11-26 18:26:58 +0100

    build: 🚀 Added upload scripts

---
+++ continuous_delivery/modrinth_config.py
@@ -0,0 +1,32 @@
+
+# Configuration for Modrinth
+from config import *
+
+# Constants
+SUMMARY: str = "A Minecraft data pack library for handling custom ore generation by using a smart system for ore location."
+
+DESCRIPTION_MARKDOWN: str = ""
+if os.path.exists(f"{ROOT}/README.md"):
+	with open(f"{ROOT}/README.md", "r", encoding="utf-8") as file:
+		DESCRIPTION_MARKDOWN = file.read()
+else:
+	print("README.md not found, description_markdown will be empty")
+
+# Dependencies (list of modrinth slugs)
+DEPENDENCIES: list[dict] = []
+
+# Version type (release, beta, alpha)
+VERSION_TYPE: str = "release"
+
+# Configuration
+modrinth_config: dict = {
+	"slug": NAMESPACE,
+	"project_name": PROJECT_NAME,
+	"version": VERSION,
+	"summary": SUMMARY,
+	"description_markdown": DESCRIPTION_MARKDOWN,
+	"dependencies": DEPENDENCIES,
+	"version_type": VERSION_TYPE,
+	"build_folder": BUILD_FOLDER,
+}
+
